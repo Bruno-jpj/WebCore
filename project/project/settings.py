b0c2127a9e824b0c6db293b88918865c2b8334af
RootBruno <lorenzoeliasb@gmail.com>
2026-03-17 10:30:54 +0100

    finished 0do some on filed-tests

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,7 +117,8 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
         "OPTIONS": {
             # resolve data validation problems
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
         },
     }
 }
